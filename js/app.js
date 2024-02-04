078691583e5de3da1899c40f02b8da410f1f2723
ShovanTheMan <shovan2192@gmail.com>
2024-02-04 18:21:47 +0600

    loader added

--- js/app.js
+++ js/app.js
@@ -34,13 +34,27 @@ const displayPhone = phone =>{
        `;
        phoneContainer.appendChild(phoneDiv);
     });
+    // stop spinner
+    toggleSpinner(false);
+
 }
 
 document.getElementById('btn-search').addEventListener('click',function(){
+  // start loader
+  toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhone(searchText);
     
 })
+const toggleSpinner = isLoading =>{
+  const loaderSection = document.getElementById('loader');
+  if(isLoading ){
+    loaderSection.classList.remove('d-none');
+  }
+  else{
+    loaderSection.classList.add('d-none');
+  }
+}
 
 loadPhone();
